34311a5b84b7eedb515ead81b807ae764529a460
Guestbyte <guestbyte@gmail.com>
2019-03-29 17:55:08 -0300

    Files Updates

--- api.php
+++ api.php
@@ -1,33 +1,53 @@
-<?php
-require_once 'meekrodb.2.3.class.php';
-DB::$user = 'mathe046_dw';
-DB::$password = 'dw_mathema';
-DB::$dbName = 'mathe046_dw';
-
-require("canvaslib.php");
-
-$api = new CanvasLMS($token, $domain);
-
-$method = $_SERVER['REQUEST_METHOD'];
-$params = explode('/', trim($_SERVER['PATH_INFO'], '/'));
-//$env = $params[0];
-$endpoint = $_SERVER['PATH_INFO'];
-
-switch ($method) {
-    case 'GET':
-        echo json_encode($api->getlist('/api/v1' . $endpoint . '?', 'id', '', -1), JSON_UNESCAPED_UNICODE);
-        die();
-        break;
-    case 'PUT':
-        echo json_encode("PUT", JSON_UNESCAPED_UNICODE);
-        die();
-        break;
-    case 'POST':
-        echo json_encode("POST", JSON_UNESCAPED_UNICODE);
-        die();
-        break;
-    case 'DELETE':
-        echo json_encode("DELETE", JSON_UNESCAPED_UNICODE);
-        die();
-        break;
+<?php
+ //require_once 'meekrodb.2.3.class.php';
+
+//DB::$user = 'mathe046_dw';
+//DB::$password = 'dw_mathema';
+//DB::$dbName = 'mathe046_dw';
+
+require("canvaslib.php");
+
+$api = new CanvasLMS($token, $domain);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$params = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+
+//$env = $params[0];
+
+$endpoint = $_SERVER['PATH_INFO'];
+
+switch ($method) {
+
+    case 'GET':
+
+        echo json_encode($api->getlist('/api/v1' . $endpoint . '?', 'id', '', -1), JSON_UNESCAPED_UNICODE);
+
+        die();
+
+        break;
+
+    case 'PUT':
+
+        echo json_encode("PUT", JSON_UNESCAPED_UNICODE);
+
+        die();
+
+        break;
+
+    case 'POST':
+
+        echo json_encode("POST", JSON_UNESCAPED_UNICODE);
+
+        die();
+
+        break;
+
+    case 'DELETE':
+
+        echo json_encode("DELETE", JSON_UNESCAPED_UNICODE);
+
+        die();
+
+        break;
 }
